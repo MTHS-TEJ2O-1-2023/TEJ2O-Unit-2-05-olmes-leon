75a0f6bdd921899127134e6135d810feebd23daa
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2023-09-26 13:19:54 -0400

    stuff

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Olmes
+ * Created on: Sep 2023
+ * This program tells the Percentage of temperature
 */
 
-basic.showString('Hello, World!')
+// variable or a random number
+
+let temperature: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+
+    temperature = input.temperature() + 273.15
+    temperature = Math.round(temperature)
+
+    basic.showString('temperature is' + (temperature).toString() )
+    basic.showString('K') 
+})
